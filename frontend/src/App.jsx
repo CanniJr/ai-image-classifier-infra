5ce42a3060853b7c3b4001ee241fef545d9fc1a0
Cannigia Laluw <cannigialaluw@gmail.com>
2025-05-28 16:18:57 -0700

    used vite react for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to the React App</h1>
+          <p>This is a simple React application.</p>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
